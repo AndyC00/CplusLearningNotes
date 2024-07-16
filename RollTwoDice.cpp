f5699f2656666873fb8e19afa73ab5564775c396
AndyC00 <chenjinzhi96@gmail.com>
2024-07-16 14:11:23 +1200

    16/07
    C++first excise

---
+++ RollTwoDice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <>
+
+void RollTwoDice(/* Insert your code here */);
+
+int main()
+{
+	// Seed the pseudo random number generate here.
+	// Declare two local integers, representing the two dice.
+	// Set each dice to hold a value of zero.
+	// Print out the contents of the two dice.
+	// Call rollTwoDice with the local dice variables.
+	// Print out the contents of the two dice after the call.
+	return 0;
+}
+void RollTwoDice(/* Insert your code here */)
+{
+	/* Insert your code here */
+}
